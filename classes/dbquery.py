2a9ba1d1f7df3b3c903624036b02c4404ce17bba
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-05-03 19:34:10 +0300

    Fixed dbquery.py

--- classes/dbquery.py
+++ classes/dbquery.py
@@ -4,16 +4,16 @@ class DatabaseQuery:
     def __init__(self):
         __db = dbConfig()
         self.connection = __db.getConnection()
-        self.cursor = self.connection.cursor()
         self.postgres = __db.postgres
 
     def execute(self, query):
         try:
-            self.cursor.execute(query)
-            queryHasReturnValue = self.cursor.description is not None
+            cursor = self.connection.cursor()
+            cursor.execute(query)
+            queryHasReturnValue = cursor.description is not None
 
             if queryHasReturnValue:
-                return self.cursor.fetchall()
+                return cursor.fetchall()
             else:
                 print("Query executed successfully.")
         except self.postgres.Error as e:
