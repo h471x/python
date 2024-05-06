550e55ed0dd7c340fffc55b028987214fed188f4
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-05-06 07:56:42 +0300

    Added fileinit class to call in setup

---
+++ classes/utils/fileinit.py
@@ -0,0 +1,6 @@
+import subprocess
+from .filecreator import FileGenerator
+
+class InitFilesGenerator:
+    def __init__(self, file):
+        self.file = file
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from classes.utils.package import PackageManager as PkgMngr
+from classes.utils.fileinit import InitFilesGenerator as initFile
 
 if __name__ == '__main__':
     pkg = PkgMngr(PkgMngr.getPkg())
