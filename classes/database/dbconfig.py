1f1b83d8f82bac3208060c12e84d319a097785c1
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-05-06 13:39:22 +0300

    Added createControllerFile method

--- classes/database/dbconfig.py
+++ classes/database/dbconfig.py
@@ -138,3 +138,64 @@ class DatabaseConfigurator:
             else:
                 # print(f"{class_file_name} already exists")
                 pass
+
+    def createControllerFile(self, createQuery):
+        table = self.getTableInfos(createQuery)
+        tableClass = newFile('app/controllers')
+
+        for tableName, attributes in table.items():
+            table_name_lower = tableName.lower()
+            class_file_name = f"{table_name_lower}.py"
+            file_path = tableClass.getFilePath(class_file_name)
+
+            # Check if the file exists and is blank (size is 0)
+            if not os.path.exists(file_path) or (os.path.exists(file_path) and os.path.getsize(file_path) == 0):
+                # Get the attributes
+                table_attributes = {attr for attr in attributes}
+
+                # Generate default data dictionary with multi-line formatting
+                # Add 6 identations = 24 spaces after each line break
+                default_data = ",\n".join([f"'{attr}': '{attr}'" for attr in attributes])
+                default_data = default_data.replace('\n', '\n' + ' ' * 24)
+
+                # Generate default newData
+                default_new_data = ",\n".join([f"'{attr}': 'new_{attr}'" for attr in attributes])
+                default_new_data = default_new_data.replace('\n', '\n' + ' ' * 24)
+
+                tableClassContent = (
+                    f"""from sys import path
+                    from os.path import abspath as abs, join as jn, dirname as dir
+                    path.append(abs(jn(dir(__file__), '..', '..')))
+
+                    from classes.tables.{table_name_lower} import {tableName}_Controller as {table_name_lower}
+
+                    {table_name_lower}_data = {{
+                        {default_data}
+                    }}
+
+                    {table_name_lower}_new_data = {{
+                        {default_new_data}
+                    }}
+
+                    {table_name_lower} = {table_name_lower}()
+
+                    def insert_template():
+                        {table_name_lower}.insert({table_name_lower}_data)
+
+                    """
+                )
+
+                # Adjust indentation for subsequent lines
+                # Remove 4 indentations = 4 x 4 spaces = 16 spaces
+                identations = 5
+                lines = tableClassContent.split('\n')
+                adjusted_lines = [lines[0]] + [line[identations*4:] for line in lines[1:]]
+                tableClassContent = '\n'.join(adjusted_lines)
+
+                tableClass.writeFile(file_path, tableClassContent)
+
+                # print(f"{tableName} {{ {', '.join(attributes)} }}")
+                print(f"New Controller File  : app/controllers/{class_file_name}")
+            else:
+                # print(f"{class_file_name} already exists")
+                pass
