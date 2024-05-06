8342f610202d6a3a72e4ae6380cb31ea73c6c53b
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-05-06 08:28:58 +0300

    Called postgres.sh in setup

--- setup.py
+++ setup.py
@@ -4,3 +4,6 @@ from classes.utils.fileinit import InitFilesGenerator as initFile
 if __name__ == '__main__':
     pkg = PkgMngr(PkgMngr.getPkg())
     pkg.install()
+
+    postgresConf = initFile('config/postgres/postgres.sh')
+    postgresConf.runShell()
