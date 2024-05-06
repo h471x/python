ecd2c11cb7a877f6cf0dee8f6c39e1ba39e605a2
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-05-06 08:28:00 +0300

    Added runShell method

--- classes/utils/fileinit.py
+++ classes/utils/fileinit.py
@@ -4,3 +4,9 @@ from .filecreator import FileGenerator
 class InitFilesGenerator:
     def __init__(self, file):
         self.file = file
+
+    def runShell(self):
+        try:
+            subprocess.check_call(['bash', self.file])
+        except subprocess.CalledProcessError as e:
+            print(f"Error executing {self.file}: {e}")
