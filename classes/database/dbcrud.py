7f39afe376e40b7910527f20dc6d50c1fca92ec7
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-05-07 23:19:42 +0300

    Fixed update method in dbquery & Code refactored

--- classes/database/dbcrud.py
+++ classes/database/dbcrud.py
@@ -56,47 +56,55 @@ class CrudHandler():
         self.db.execute(executeQuery)
 
     def insert(self, data):
-        __firstColumn = self.__getFirstColumn(data)
-        __firstValue = self.__getFirstValue(data)
-        __insert_or_ignore_query = f"""
+        firstColumn = self.__getFirstColumn(data)
+        firstValue = self.__getFirstValue(data)
+
+        self.db.execute(f"""
             INSERT INTO {self.table} ({self.__getColumn(data)})
             SELECT {self.__getValues(data)}
             WHERE NOT EXISTS(
                 SELECT 1 FROM {self.table}
-                WHERE {__firstColumn} = '{__firstValue}'
+                WHERE {firstColumn} = '{firstValue}'
             );
         """
-        self.db.execute(__insert_or_ignore_query)
+        )
 
     def selectAll(self):
         return self.db.execute(f"""
             SELECT * from {self.table};
-        """)
+        """
+        )
 
     def select(self, condition):
         return self.db.execute(f"""
             SELECT * FROM {self.table}
             WHERE {self.__getCondition(condition)};
-        """)
+        """
+        )
 
     def update(self, oldData, newData):
+        newFirstColumn = self.__getFirstColumn(newData)
+        newFirstValue = self.__getFirstValue(newData)
+
         self.db.execute(f"""
             UPDATE {self.table} SET {self.__getSetValues(newData)}
             WHERE {self.__getCondition(oldData)}
             AND NOT EXISTS (
                 SELECT 1 FROM {self.table}
-                WHERE {self.__getFirstColumn(oldData)} = '{self.__getFirstValue(newData)}'
+                WHERE {newFirstColumn} = '{newFirstValue}'
             );
         """
-    )
+        )
 
     def deleteAll(self):
         self.db.execute(f"""
             DELETE from {self.table};
-        """)
+        """
+        )
 
     def delete(self, condition):
         self.db.execute(f"""
             DELETE FROM {self.table}
             WHERE {self.__getCondition(condition)};
-        """)
+        """
+        )
