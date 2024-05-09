ac9d5133eb4e7cb57bdbf10777c130f09d95b42d
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-05-09 15:21:29 +0300

    Added methods to ctkwindow class

--- classes/window/ctkwindow.py
+++ classes/window/ctkwindow.py
@@ -1,14 +1,28 @@
-import customtkinter as ctk
+import customtkinter
 
 class CtkWindow:
     def __init__(self):
-        self.window = ctk.CTk()
+        self.ctk = customtkinter
+        self.window = self.ctk.CTk()
         ctk.set_appearance_mode("System")
         ctk.set_default_color_theme("blue")
 
     def maximise(self):
         self.window.state('zoomed')
 
+    def setSize(self, width, height):
+        self.window.geometry(f"{width}x{height}")
+
+    def open(self):
+        self.window.mainloop()
+
     def openMaximised(self):
         self.window.after(0, lambda:self.maximise())
-        self.window.mainloop()
+        self.open()
+
+    def newButton(self, parent, content, action):
+        return self.ctk.CTkButton(
+            master=parent,
+            text=f"{content}",
+            command=action
+        )
