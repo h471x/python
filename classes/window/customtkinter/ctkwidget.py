693ea185538a34fdabdaed392ec7e0c1b4488ff5
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-05-15 16:13:44 +0300

    Implemented a new class CtkWidget

--- classes/window/customtkinter/ctkwidget.py
+++ classes/window/customtkinter/ctkwidget.py
@@ -1,5 +1,12 @@
 import customtkinter
 
 class CtkWidget:
-    def __init__(self, parent):
+    def __init__(self):
+        self.ctk = customtkinter
 
+    def newButton(self, parent, text, btnCommand):
+        return self.ctk.CTkButton(
+            master=parent,
+            text=f"{text}",
+            command=btnCommand
+        )
