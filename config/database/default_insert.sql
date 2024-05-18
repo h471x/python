48462f04212caaa9f62a860370880a67caf39b78
Hernandez <randrianirinahernandezstephan@gmail.com>
2024-05-18 08:43:55 +0300

    The default value of student table is ready

---
+++ config/database/default_insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO student (
+  id_card,
+  first_name,
+  last_name,
+  birth,
+  gender,
+  adress,
+  phone,
+  student_id,
+  major,
+  level,
+  cursus
+) VALUES ( 
+  '0123456789',
+  'Marc',
+  'Parker',
+  '2003-05-22',
+  'M',
+  'New York',
+  '0123456789',
+  '0006',
+  'Computer science',
+  'M2',
+  'Cyber security'
+  );
