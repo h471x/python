1d77b157f99b80225f82ac9140cd354496037495
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-06-21 16:57:13 +0300

    Shrinking the Sidebar

--- assets/styles/defaults.py
+++ assets/styles/defaults.py
@@ -4,7 +4,25 @@ def configure_styles():
     style = ttk.Style()
 
     # Configure Treeview headers
-    style.configure("Treeview.Heading", font=("Arial", 12, "bold"), padding=[10, 10, 10, 10])
+    style.configure(
+        "Treeview.Heading",
+        font=("Arial", 12, "bold"),
+        padding=[10, 10, 10, 10],
+        background="#000000",
+        foreground="#FFFFFF",
+    )
 
     # Configure Treeview rows
-    style.configure("Treeview", font=("Arial", 12), rowheight=40)  # Adjust row height as needed
+    style.configure(
+        "Treeview",
+        font=("Arial", 12),
+        rowheight=40,
+        background="#202020",
+        foreground="#FFFFFF",
+        fieldbackground="#202020"
+    )
+
+    # Configure the style for selected row
+    # style.map("Treeview",
+    #           background=[("selected", "#347083")],  # Background color when selected
+    #           foreground=[("selected", "#FFFFFF")])  # Foreground color when selected
