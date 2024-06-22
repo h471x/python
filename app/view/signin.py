c26886d1b174b32898f20910e981ed4489ae2c73
Hernandez <randrianirinahernandezstephan@gmail.com>
2024-06-22 11:11:53 +0300

    Address field added

--- app/view/signin.py
+++ app/view/signin.py
@@ -32,35 +32,40 @@ def signup_ui ():
     signup_label.pack(expand=True,fill="both", padx=10, pady=10)
 
     #second frame for body that contains the input widgets
+    #First name
     body = widget.new_frame(signup.window, "transparent", 5)
     body.pack(expand=True, fill="both", padx=10, pady=10)
     firstname_label=widget.new_label(body,"First name :",font=("Roboto",20))
     firstname_label.pack(padx=10,pady=10)
     firstname_input=widget.new_input(body,"grey",placeholder_text="First name",font=("Roboto",15),corner_radius=10)
     firstname_input.pack(padx=10,pady=10)
-
+    #Last name
     lastname_label=widget.new_label(body,"Last name :",font=("Roboto",20))
     lastname_label.pack(padx=10,pady=10)
     lastname_input=widget.new_input(body,"grey",placeholder_text="Last Name",font=("Roboto",15),corner_radius=10)
     lastname_input.pack(padx=10,pady=10)
-
+    #Birth
     birth_label=widget.new_label(body,"Birth :",font=("Roboto",20))
     birth_label.pack(padx=10,pady=10)
     #Calendar
     calendar_view=DateEntry(body,year=2024,month=6,day=24)
     calendar_view.pack()
-
+    #Gender
     gender_label=widget.new_label(body,"Gender :",font=("Roboto",20))
     gender_label.pack(padx=10,pady=10)
     gender=tkinter.StringVar(value="")
-    male_radiobutton=customtkinter.CTkRadioButton(body, text="Male",command=radiobutton_event,variable=gender,value='m')
-    female_radiobutton=customtkinter.CTkRadioButton(body, text="Female",command=radiobutton_event,variable=gender,value='f')
+    male_radiobutton=customtkinter.CTkRadioButton(body, text="Male",command=radiobutton_event,variable=gender,value='M')
+    female_radiobutton=customtkinter.CTkRadioButton(body, text="Female",command=radiobutton_event,variable=gender,value='F')
     male_radiobutton.pack()
     female_radiobutton.pack()
+    #Address
+    address_label=widget.new_label(body,"address :",font=("Roboto",15))
+    address_label.pack(padx=10,pady=10)
+    address_input=widget.new_input(body,"grey","address",placeholder_text="address")
+    address_input.pack(padx=10,pady=10)
     
 
     signup.open_centered()
 
 if __name__ == '__main__':
-    G="Nothing"
     signup_ui()
