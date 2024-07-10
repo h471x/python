ebcda38577090dc4769bc0b7d5dc4222992e151e
Hernandez <randrianirinahernandezstephan@gmail.com>
2024-07-10 06:01:31 +0300

    Adding new SELECT query for Student Page

--- config/database/sql/query.sql
+++ config/database/sql/query.sql
@@ -6,3 +6,11 @@ SELECT s.student_id AS Matricule ,CONCAT(s.first_name,' ',s.last_name) AS Nom ,c
   ON g.fk_cename=ce.cename
   INNER JOIN teacher AS t 
   ON g.fk_teacher_id=t.teacher_id;
+
+
+-- SELECT FOR STUDENT PAGE
+SELECT s.student_id as Matricule,CONCAT(s.first_name,' ',s.last_name) AS Nom ,(g.theorical_grade + g.pratical_grade )/2 AS Note FROM grade AS g 
+INNER JOIN student AS s
+ON s.student_id=g.fk_student_id
+WHERE s.level=value1 AND g.fk_cename=value2
+ORDER BY s.student_id ASC;
